33965115b3a748dbf5e20552e2fd827d57cb7459
yangsoje <didthwp@gmail.com>
2026-09-22 17:10:05 +0900

    feat: 2부터 100까지 소수 판별 기본 로직 구현

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-# prime-number.py - 소수 구하기 프로그램 기본 틀
+# prime-number.py - 소수 구하기 기본 로직 완성
 print("1부터 100 사이의 소수를 구합니다.")
-for num in range(1, 101):
- pass # 기본 반복문 틀 작성
+for num in range(2, 101):
+  is_prime = True
+  for i in range(2, num):
+   if num % i == 0:
+    is_prime = False
+    break
+  if is_prime:
+   print(num, end=" ")
